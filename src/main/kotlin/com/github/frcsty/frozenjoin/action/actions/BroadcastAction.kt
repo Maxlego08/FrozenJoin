df6816d2a838d52824d0931287ffdfa8d0104283
Vaishnav <vaishnav.anil@rocketmail.com>
2020-07-22 18:32:50 +0530

    Cleaned up Actions

--- src/main/kotlin/com/github/frcsty/frozenjoin/action/actions/BroadcastAction.kt
+++ src/main/kotlin/com/github/frcsty/frozenjoin/action/actions/BroadcastAction.kt
@@ -3,8 +3,7 @@ package com.github.frcsty.frozenjoin.action.actions
 import org.bukkit.entity.Player
 
 class BroadcastAction : Action {
-    override val id: String
-        get() = "BROADCAST"
+    override val id: String = "BROADCAST"
 
     override fun run(player: Player, data: String) {
         for (i in 0..4) {
--- src/main/kotlin/com/github/frcsty/frozenjoin/action/actions/MessageAction.kt
+++ src/main/kotlin/com/github/frcsty/frozenjoin/action/actions/MessageAction.kt
@@ -3,8 +3,7 @@ package com.github.frcsty.frozenjoin.action.actions
 import org.bukkit.entity.Player
 
 class MessageAction : Action {
-    override val id: String
-        get() = "MESSAGE"
+    override val id: String = "MESSAGE"
 
     override fun run(player: Player, data: String) {
         println(data)
