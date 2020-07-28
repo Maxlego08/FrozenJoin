9916cbd09fc3d9919c72bbcad5013d51f888b67c
Alexander Wood <alexljwood24@hotmail.co.uk>
2020-07-28 17:12:24 +0100

    Implement TitleBroadcastAction and TitleMessageAction

--- src/main/kotlin/com/github/frcsty/frozenjoin/action/actions/TitleBroadcastAction.kt
+++ src/main/kotlin/com/github/frcsty/frozenjoin/action/actions/TitleBroadcastAction.kt
@@ -1,12 +1,34 @@
 package com.github.frcsty.frozenjoin.action.actions
 
+import com.github.frcsty.frozenjoin.util.getRecipients
+import com.github.frcsty.frozenjoin.util.sendPlayerTitle
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 
 object TitleBroadcastAction : Action {
-
     override val id = "TITLEBROADCAST"
 
     override fun run(player: Player, data: String) {
+        val args = data.split(" ")
 
+        val (scope, title, subtitle) = args
+        val fadeIn = args.getOrNull(3)?.toIntOrNull() ?: DEFAULT_FADE_IN
+        val stay = args.getOrNull(4)?.toIntOrNull() ?: DEFAULT_STAY
+        val fadeOut = args.getOrNull(5)?.toIntOrNull() ?: DEFAULT_FADE_OUT
+
+        val recipients = when (scope) {
+            "PLAYER" -> setOf(player)
+            "WORLD", "REGION" -> player.getRecipients(scope)
+            else -> Bukkit.getOnlinePlayers().toSet()
+        }
+
+        recipients.forEach {
+            it.sendPlayerTitle(title, subtitle, fadeIn, stay, fadeOut)
+        }
     }
+
+
+    private const val DEFAULT_FADE_IN = 5
+    private const val DEFAULT_STAY = 10
+    private const val DEFAULT_FADE_OUT = 5
 }
--- src/main/kotlin/com/github/frcsty/frozenjoin/action/actions/TitleMessageAction.kt
+++ src/main/kotlin/com/github/frcsty/frozenjoin/action/actions/TitleMessageAction.kt
@@ -7,6 +7,8 @@ object TitleMessageAction : Action {
     override val id = "TITLE"
 
     override fun run(player: Player, data: String) {
+        val args = data.split(" ")
 
+        TitleBroadcastAction.run(player, (listOf("PLAYER") + args.drop(1)).joinToString(" "))
     }
 }
