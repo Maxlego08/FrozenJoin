798b1f5e7838056bd4340b440e95524f0339d13f
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2022-10-01 11:20:22 +0300

    Upgraded kotlin version to latest release

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("com.github.johnrengelman.shadow") version "7.1.2"
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.7.20"
 }
 
 group = "com.github.frcsty"
