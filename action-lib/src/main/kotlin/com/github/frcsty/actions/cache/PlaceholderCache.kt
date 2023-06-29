8d6be672579b4d8f6e09948e300f080799b6404f
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2023-06-29 23:42:18 +0300

    Added relational placeholder cache

--- action-lib/src/main/kotlin/com/github/frcsty/actions/cache/PlaceholderCache.kt
+++ action-lib/src/main/kotlin/com/github/frcsty/actions/cache/PlaceholderCache.kt
@@ -5,10 +5,10 @@ import org.bukkit.entity.Player
 
 interface PlaceholderCache {
     fun updatePlaceholders()
-    fun updatePlaceholders(uuid: UUID)
-    fun updatePlaceholders(player: Player)
+    fun updatePlaceholders(uuid: UUID, uuid2: UUID? = null)
+    fun updatePlaceholders(player: Player, player2: Player? = null)
     fun removePlayer(uuid: UUID)
     fun removePlayer(player: Player)
-    fun setPlaceholders(message: String, player: Player): String
-    fun setPlaceholders(message: String, uuid: UUID): String
+    fun setPlaceholders(message: String, uuid: UUID, uuid2: UUID? = null): String
+    fun setPlaceholders(message: String, player: Player, player2: Player? = null): String
 }
--- src/main/kotlin/com/github/frcsty/cache/PlaceholderCache.kt
+++ src/main/kotlin/com/github/frcsty/cache/PlaceholderCache.kt
@@ -16,42 +16,78 @@ import org.bukkit.scheduler.BukkitRunnable
 
 class PlaceholderCache(private val plugin: Plugin, private val settings: Settings) : PlaceholderCache, Listener,
     BukkitRunnable() {
-    private val cache = ConcurrentHashMap<UUID, Map<String, String>>()
+
+    // for each player hold a map of simple placeholders and their values
+    private val simplePlaceholdersCache = ConcurrentHashMap<UUID, Map<String, String>>()
+
+    // for each player pair hold a map of relational placeholders and their values
+    private val relationalPlaceholdersCache = ConcurrentHashMap<UUID, ConcurrentHashMap<UUID, Map<String, String>>>()
+
 
     override fun updatePlaceholders() {
-        Bukkit.getOnlinePlayers().forEach { updatePlaceholders(it) }
+        Bukkit.getOnlinePlayers().forEach { player1 ->
+            Bukkit.getOnlinePlayers().forEach secondLoop@{ player2 ->
+                if (player1.uniqueId == player2.uniqueId) return@secondLoop
+                updatePlaceholders(player1, player2)
+            }
+        }
     }
 
-    override fun updatePlaceholders(uuid: UUID) {
-        Bukkit.getPlayer(uuid)?.let { updatePlaceholders(it) }
+    override fun updatePlaceholders(uuid: UUID, uuid2: UUID?) {
+        Bukkit.getPlayer(uuid)?.let { player -> updatePlaceholders(player, uuid2?.let { Bukkit.getPlayer(it) }) }
     }
 
-    override fun updatePlaceholders(player: Player) {
-        cache.remove(player.uniqueId)
-        cache[player.uniqueId] =
+    override fun updatePlaceholders(player: Player, player2: Player?) {
+        // Clear the cache
+        simplePlaceholdersCache.remove(player.uniqueId)
+        relationalPlaceholdersCache.remove(player.uniqueId)
+
+        // Update the simple placeholders cache
+        simplePlaceholdersCache[player.uniqueId] =
             settings.cachedPlaceholders.associateWith { PlaceholderAPI.setPlaceholders(player, it) }
+
+        // Update the relational placeholders cache
+        player2?.let {
+            relationalPlaceholdersCache.getOrDefault(player.uniqueId, ConcurrentHashMap())[player2.uniqueId] =
+                settings.cachedRelationalPlaceholders.associateWith {
+                    PlaceholderAPI.setRelationalPlaceholders(player, player2, it)
+                }
+        }
     }
 
     override fun removePlayer(uuid: UUID) {
-        cache.remove(uuid)
+        simplePlaceholdersCache.remove(uuid)
+        relationalPlaceholdersCache.remove(uuid)
+        relationalPlaceholdersCache.forEach { it.value.remove(uuid) }
     }
 
     override fun removePlayer(player: Player) {
         removePlayer(player.uniqueId)
     }
 
-    override fun setPlaceholders(message: String, player: Player): String {
-        return setPlaceholders(message, player.uniqueId)
-    }
+    override fun setPlaceholders(message: String, uuid: UUID, uuid2: UUID?): String {
+        val simplePlaceholders = simplePlaceholdersCache[uuid]
+        val relationalPlaceholders = relationalPlaceholdersCache[uuid]
 
-    override fun setPlaceholders(message: String, uuid: UUID): String {
-        val placeholders = cache[uuid] ?: return message
+        if (simplePlaceholders == null && relationalPlaceholders == null) {
+            return message
+        }
 
         var newMessage = message
-        placeholders.forEach {
+
+        simplePlaceholders?.forEach {
             newMessage = newMessage.replace(it.key, it.value)
         }
+
+        relationalPlaceholders?.get(uuid2)?.forEach {
+            newMessage = newMessage.replace(it.key, it.value)
+        }
+
         return newMessage
+    }
+
+    override fun setPlaceholders(message: String, player: Player, player2: Player?): String {
+        return setPlaceholders(message, player.uniqueId, player2?.uniqueId)
     }
 
     override fun run() {
